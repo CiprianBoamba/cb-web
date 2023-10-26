d3866f1c9ea5c5ab1c558490b9ad99e1abcffcdb
CiprianBoamba <ciprian.boamba@gmail.com>
2023-10-26 13:57:55 +0100

    add words-animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
+import { ComputersCanvas } from './canvas';
 
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+  const [text, setText] = useState('');
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [currentTextIndex, setCurrentTextIndex] = useState(0);
 
-export default Hero
+  const texts = ['Ciprian', 'Web Developer'];
+
+  useEffect(() => {
+    const targetText = texts[currentTextIndex];
+
+    let timeout;
+    if (isDeleting) {
+      if (text.length > 0) {
+        timeout = setTimeout(() => {
+          setText(text.slice(0, -1));
+        }, 100);
+      } else {
+        setIsDeleting(false);
+        setCurrentTextIndex((currentTextIndex + 1) % texts.length);
+      }
+    } else {
+      if (text.length < targetText.length) {
+        timeout = setTimeout(() => {
+          setText(targetText.slice(0, text.length + 1));
+        }, 200);
+      } else {
+        timeout = setTimeout(() => {
+          setIsDeleting(true);
+        }, 2000);
+      }
+    }
+
+    return () => clearTimeout(timeout);
+  }, [text, isDeleting, currentTextIndex]);
+
+  const textClass =
+    texts[currentTextIndex] === 'Ciprian'
+      ? 'text-ciprian'
+      : 'text-webdeveloper';
+
+  return (
+    <section className={`relative w-full h-screen mx-auto`}>
+      <div
+        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+      >
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>Hello, I'm</h1>
+
+          <h1 className={`${styles.heroHeadWords} ${textClass}`}>
+            {text}
+            <span className="inline-block bg-black text-white w-2 ml-1 animate-blink">
+              |
+            </span>
+          </h1>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
